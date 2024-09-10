4119727af583c7ae14eb6a2280f38a90247b1fcd
GrzybowskiPiotr <grzybowski_piotr@hotmail.com>
2024-09-10 08:46:43 +0200

    zadanie

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,24 @@
-const a = '4.2';
-const b = 9;
+const a = "2.5";
+const b = 4;
 
 console.log(a, b);
+console.log(typeof a);
+console.log(typeof b);
+
+let c = Number(a) + Number(a);
+let d = Number(a) + b;
+let e = a - b;
+let f = b - b;
+let g = a / b;
+let h = a * b;
+let i = a % b;
+
+const results = [a, b, c, d, e, f, g, h, i];
+
+results.map((result) => {
+  if (result >= 20) {
+    console.log(`operacja większa od 20. Wynik to ${result}`);
+  } else {
+    console.log(`operacja mniejsza od 20. Wynik to ${result}`);
+  }
+});
