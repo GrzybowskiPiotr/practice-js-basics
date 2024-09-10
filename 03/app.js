ef1c71f7e76533edf36436c4606bfbfe899913ce
GrzybowskiPiotr <grzybowski_piotr@hotmail.com>
2024-09-10 15:10:08 +0200

    rozwiązanie zadania 03

--- 03/app.js
+++ 03/app.js
@@ -7,12 +7,50 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+function getSum(a, b, c) {
+  const NumberArray = [
+    parseInt(Number(a)),
+    parseInt(Number(b)),
+    parseInt(Number(c)),
+  ];
 
+  const tempArray = NumberArray.filter(
+    (number) => number !== Math.max(...NumberArray)
+  );
 
+  return Math.max(...NumberArray) + Math.max(...tempArray);
+}
 
+const isEven = function (a) {
+  if (!isNaN(a)) {
+    return a % 2 == 0 ? true : false;
+  } else {
+    return null;
+  }
+};
 
-
+function showInfo(val, type) {
+  switch (type) {
+    case null:
+      console.log(`Podany argunemt ${val} nie jest liczbą`);
+      break;
+    case true:
+      console.log(`Podany argunemt ${val} jest parzysty`);
+      break;
+    case false:
+      console.log(`Podany argunemt ${val} jest nieprzysty`);
+      break;
+    default:
+      console.log("default");
+  }
+}
 
 function randomNumber(min, max) {
-    return Math.round((Math.random() * (max - min)) + min);
+  return Math.round(Math.random() * (max - min) + min);
 }
+
+const sum = getSum(a, b, c);
+
+const even = isEven(sum);
+
+showInfo(sum, even);
