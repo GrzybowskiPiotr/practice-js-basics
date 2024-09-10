8051bd23bf38251d8d9d1bff5f17efa071b96f34
GrzybowskiPiotr <grzybowski_piotr@hotmail.com>
2024-09-10 13:01:20 +0200

    rozwiązanie zadania

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,31 @@
-
 /* rozwiązanie z pętlą for */
-const x = 4;
 
-
-
+let givenNumber = prompt("Podaj liczbę!", 4);
+if (givenNumber > 0 && givenNumber <= 9) {
+  for (let i = 1; i <= 9; i++) {
+    console.log(`${i} * ${givenNumber} = ${i * givenNumber}`);
+  }
+} else {
+  alert("Podaj liczbę z zakresu 1 - 9");
+  window.location.reload();
+}
 /* rozwiązanie z pętlą while  */
+
+let a = prompt("Podaj podstawę", 1);
+let n = prompt("Podaj wykładnik", 2);
+let result;
+let i = 0;
+let str = "";
+
+if (!isNaN(a) && !isNaN(n)) {
+  result = 1;
+  while (i < n) {
+    result = result * a;
+    str = a + " * " + str;
+    i++;
+  }
+  console.log(`Wynik potęgowania: ${str.slice(0, -2)}= ${result}`);
+} else {
+  alert("Wprowadź wartości liczbowe");
+  window.location.reload();
+}
